d75aba446d9b3c0575ba9395f23edeb474e0d184
Burgener francois <francois.burgener@heig-vd.ch>
2020-05-01 13:39:09 +0200

    minor change

--- labo/helloword.py
+++ labo/helloword.py
@@ -4,7 +4,7 @@ import vtk as vtk
 def readFile(filename):
     with open(filename, 'r', encoding="utf-8") as fd:
         points = vtk.vtkPoints()
-        fd.readline()
+        lengths = fd.readline().split(' ')
 
         x = 0
         y = 0
@@ -21,32 +21,34 @@ def readFile(filename):
             y += 1
             x = 0
 
-        return points
+        return points, int(lengths[0]), int(lengths[1])
 
 
 def main():
     print('Data acquisition...')
-    points = readFile('altitudes.txt')
+    points, length_x, length_y = readFile('altitudes.txt')
 
     print(points)
 
-    dims = [3001, 3001, 1]
+    dims = [length_x, length_y, 1]
 
     grid = vtk.vtkStructuredGrid()
     grid.SetPoints(points)
     grid.SetDimensions(dims)
 
     lut = vtk.vtkLookupTable()
-    lut.SetHueRange(0.0, 0.667)
+    lut.SetHueRange(0.0, 1)
 
 
     gridFilter = vtk.vtkStructuredGridGeometryFilter()
     gridFilter.SetInputData(grid)
+    gridFilter.Update()
 
     # Mapper
     gridMapper = vtk.vtkDataSetMapper()
     gridMapper.SetInputConnection(gridFilter.GetOutputPort())
     gridMapper.SetLookupTable(lut)
+
     # Actor
     gridActor = vtk.vtkActor()
     gridActor.SetMapper(gridMapper)
