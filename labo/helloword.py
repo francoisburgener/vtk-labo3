033510780eaf5cc8e53e9223727f40e810a9922e
Burgener francois <francois.burgener@heig-vd.ch>
2020-05-02 19:37:08 +0200

    restructuration du code

--- labo/helloword.py
+++ labo/helloword.py
@@ -1,72 +1,108 @@
 import vtk as vtk
 
 
-def readFile(filename):
+points = vtk.vtkPoints()
+sgrid = vtk.vtkStructuredGrid()
+
+def readInFile(filename):
     with open(filename, 'r', encoding="utf-8") as fd:
-        points = vtk.vtkPoints()
-        lengths = fd.readline().split(' ')
-
-        x = 0
-        y = 0
-
+        dimensions = fd.readline().split(' ')
         lines = fd.readlines()
 
-        print(len(lines))
+        data = []
+
         for line in lines:
             splits = line.split(' ')[:-1]
-
             for split in splits:
-                points.InsertNextPoint([x, y, float(split)*0.04])
-                x += 1
-            y += 1
-            x = 0
+                data.append(float(split))
 
-        return points, int(lengths[0]), int(lengths[1])
+        return data, int(dimensions[0]), int(dimensions[1])
 
+
+def initilizeData():
+    data, Xs, Ys = readInFile('altitudes.txt')
+    scalars = vtk.vtkFloatArray()
+
+
+    # TODO comment detecter les lac ?
+
+    for i in range(0, Xs):
+        for j in range(0, Ys):
+            index = j + (i * Xs)
+
+            # TODO calcule courbre de la terre
+            points.InsertNextPoint(i, j, data[index]*0.04)
+
+            # TODO calcule du scalar
+            scalars.InsertNextValue(data[index])
+
+    sgrid.SetPoints(points)
+    sgrid.SetDimensions([Xs, Ys, 1])
+    sgrid.GetPointData().SetScalars(scalars)
+
+    # TODO ecrire les donnée dans un ficher ? comme pour le cube
+    
 
 def main():
-    print('Data acquisition...')
-    points, length_x, length_y = readFile('altitudes.txt')
 
-    print(points)
+    initilizeData()
 
-    dims = [length_x, length_y, 1]
 
-    grid = vtk.vtkStructuredGrid()
-    grid.SetPoints(points)
-    grid.SetDimensions(dims)
-
+    # TODO modifier le lut pour afficher les bonnes couleurs : Ici c'est de la merde se que j'ai fait
     lut = vtk.vtkLookupTable()
-    lut.SetHueRange(0.0, 1)
+    lut.SetNumberOfColors(10)
+    lut.SetTableRange(0, 4783)
+    lut.SetHueRange(0.6, 0.0)
+    lut.SetSaturationRange(0.5, 0.0)
+    lut.SetValueRange(0.6, 1.0)
+    lut.SetBelowRangeColor(0.0, 0.0, 1.0, 1.0)
+    lut.UseBelowRangeColorOn()
+    lut.Build()
 
 
+    # Filter pour récupérer les donnée du sgrid
     gridFilter = vtk.vtkStructuredGridGeometryFilter()
-    gridFilter.SetInputData(grid)
+    gridFilter.SetInputData(sgrid)
     gridFilter.Update()
 
     # Mapper
     gridMapper = vtk.vtkDataSetMapper()
     gridMapper.SetInputConnection(gridFilter.GetOutputPort())
+    gridMapper.SetScalarRange(1, 2400) # TODO quel range mettre ?
     gridMapper.SetLookupTable(lut)
 
     # Actor
     gridActor = vtk.vtkActor()
     gridActor.SetMapper(gridMapper)
-        # # Render
+
+    # Render
     renderer = vtk.vtkRenderer()
     renderer.AddActor(gridActor)
-    renderer.SetBackground(0.3, 0.3, 0.4)
+    renderer.SetBackground(0, 0, 0)
 
 
     renWin = vtk.vtkRenderWindow()
     renWin.AddRenderer(renderer)
-    renWin.SetSize(640, 480)
+    renWin.SetSize(640, 640)
 
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetRenderWindow(renWin)
+    iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+
+    renWin.Render()
+
+    # Genere le fichier png
+    w2if = vtk.vtkWindowToImageFilter()
+    w2if.SetInput(renWin)
+    w2if.Update()
+
+    writer = vtk.vtkPNGWriter()
+    writer.SetFileName("map.png")
+    writer.SetInputConnection(w2if.GetOutputPort())
+    writer.Write()
 
     # Interact with the data.
-    renWin.Render()
+    iren.Initialize()
     iren.Start()
 
 main()
