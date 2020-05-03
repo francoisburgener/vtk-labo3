194f13baae1888bb9a7388c2ef4ab86b9d585c8d
Burgener francois <francois.burgener@heig-vd.ch>
2020-05-03 17:20:04 +0200

    .

--- labo/helloword.py
+++ labo/helloword.py
@@ -6,6 +6,24 @@ LAT_2 = 47.5
 LNG_1 = 5.0
 LNG_2 = 7.5
 R_EARTH = 6371009
+MAX_SCALAR = 2000
+
+SEA_LVL = 370 # You can change to 370 or 0
+
+def GetScalarValue(data,Xs,Ys, i, j):
+
+    altitude = data[i][j]
+
+    testRange = (Xs - 1 > i > 0) and (Ys - 1 > j > 0)
+
+    if testRange and altitude == \
+            data[i + 1][j] == data[i - 1][j] == \
+            data[i][j + 1] == data[i][j - 1] == \
+            data[i + 1][j + 1] == data[i + 1][j - 1] == \
+            data[i - 1][j + 1] == data[i - 1][j - 1]:
+        return 0
+    else:
+        return altitude
 
 def coordinate_earth(lat,lng,alt):
     transform = vtk.vtkTransform()
@@ -20,13 +38,11 @@ def readInFile(filename):
     with open(filename, 'r', encoding="utf-8") as fd:
         dimensions = fd.readline().split(' ')
         lines = fd.readlines()
-
         data = []
 
         for line in lines:
-            splits = line.split(' ')[:-1]
-            for split in splits:
-                data.append(float(split))
+            splits = list(map(float,line.split(' ')[:-1]))
+            data.append(splits)
 
         return data, int(dimensions[0]), int(dimensions[1])
 
@@ -40,10 +56,7 @@ def main():
     delta_long = (LNG_2 - LNG_1) / Xs
     delta_lat = (LAT_2 - LAT_1) / Ys
 
-    minScalar = min(data)
-    maxScalar = max(data)
-
-    # TODO comment detecter les lac ?
+    minScalar = float("inf")
 
     for i in range(0, Xs):
         for j in range(0, Ys):
@@ -52,24 +65,27 @@ def main():
             #Calcul of latitude, longitude for each point
             latitude = LAT_1 + i * delta_lat
             longitude = LNG_1 + j * delta_long
-            altitude = data[index]
+            altitude = data[i][j]
+
+            if altitude < SEA_LVL:
+                data[i][j] = 0
 
             points.InsertNextPoint(coordinate_earth(latitude, longitude, altitude))
 
             # TODO modifier la valeur du scalar en fonction de si c'est un lac ou pas
-            scalars.InsertNextValue(altitude)
+            scalars.InsertNextValue(GetScalarValue(data, Xs, Ys, i, j))
+
+            if minScalar > altitude:
+                minScalar = altitude
 
     sgrid.SetPoints(points)
     sgrid.SetDimensions([Xs, Ys, 1])
     sgrid.GetPointData().SetScalars(scalars)
 
-    # TODO modifier le lut pour afficher les bonnes couleurs : Ici c'est de la merde se que j'ai fait
     lut = vtk.vtkLookupTable()
-    lut.SetNumberOfColors(256)
-    lut.SetTableRange(0, 4783)
-    lut.SetHueRange(0.6, 0.0)
+    lut.SetHueRange(0.4, 0.0)
     lut.SetSaturationRange(0.5, 0.0)
-    lut.SetValueRange(0.6, 1.0)
+    lut.SetValueRange(0.5, 1.0)
     lut.SetBelowRangeColor(0.0, 0.0, 1.0, 1.0)
     lut.UseBelowRangeColorOn()
     lut.Build()
@@ -83,7 +99,7 @@ def main():
     # Mapper
     gridMapper = vtk.vtkDataSetMapper()
     gridMapper.SetInputConnection(gridFilter.GetOutputPort())
-    gridMapper.SetScalarRange(minScalar, maxScalar) # TODO quel range mettre ?
+    gridMapper.SetScalarRange(minScalar, MAX_SCALAR) # TODO quel range mettre ?
     gridMapper.SetLookupTable(lut)
 
     # Actor
@@ -99,12 +115,16 @@ def main():
     renWin = vtk.vtkRenderWindow()
     renWin.AddRenderer(renderer)
     renWin.SetSize(640, 480)
+    renWin.Render()
+
+    # renderer.GetActiveCamera().SetClippingRange(1, 640 * 480)
+    # renderer.GetActiveCamera().SetFocalPoint(coordinate_earth((LAT_2 - LAT_1) / 2.0, (LNG_2 - LNG_1) / 2.0, minScalar))
+    # renderer.GetActiveCamera().SetPosition(coordinate_earth((LAT_2 - LAT_1) / 2.0, (LNG_2 - LNG_1) / 2.0, 50000))
 
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetRenderWindow(renWin)
     iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
 
-    renWin.Render()
 
     # Genere le fichier png
     w2if = vtk.vtkWindowToImageFilter()
@@ -112,11 +132,14 @@ def main():
     w2if.Update()
 
     writer = vtk.vtkPNGWriter()
-    writer.SetFileName("map.png")
+    writer.SetFileName("map{}.png".format(SEA_LVL))
     writer.SetInputConnection(w2if.GetOutputPort())
     writer.Write()
 
     print("Finish")
+    print(renderer.GetActiveCamera().GetFocalPoint())
+    print(renderer.GetActiveCamera().GetPosition())
+    print(renderer.GetActiveCamera().GetDistance())
 
     # Interact with the data.
     iren.Initialize()
